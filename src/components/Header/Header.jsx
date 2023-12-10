4e39e8841f2073c6033ae730bf555e2c5b2b8245
RudrakshGupta0413 <rudrakshgupta40@gmail.com>
2023-12-10 20:16:30 +0530

    Navbar Updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,44 @@
+// Header.js
 
+import { useState } from 'react';
 import './Header.css';
 
 const Header = () => {
+  const [selectedCompany, setSelectedCompany] = useState('Company A'); // Initial company selection
+
+  const handleCompanyChange = (company) => {
+    setSelectedCompany(company);
+  };
+
+  const handleButtonClick = () => {
+    // Handle button click logic here
+    console.log('Button clicked');
+  };
+
   return (
     <div className="header">
-      {/* Your header content goes here */}
+      <div className="center-section">
+        <img src="path/to/your/logo.png" alt="Company Logo" className="logo" />
+        <div className="company-dropdown">
+          <label>Company:</label>
+          <select value={selectedCompany} onChange={(e) => handleCompanyChange(e.target.value)}>
+            <option value="Company A">Company A</option>
+            <option value="Company B">Company B</option>
+            {/* Add more company options as needed */}
+          </select>
+        </div>
+        <div className="company-name">{selectedCompany}</div>
+      </div>
+      <div className='right-section'>
+        <div className='icon-bar'>
+            <img src='' alt='notification icon' className='icon'></img>
+        </div>
+        <div className='button'>
+            <button>
+                Post a Job
+            </button>
+        </div>
+      </div>
     </div>
   );
 };
